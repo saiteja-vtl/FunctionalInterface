d665f663f8e25532a75375ed8c80e0bd61a34bb0
saiteja-vtl <saiteja@vebytechlabs.onmicrosoft.com>
2024-07-22 09:06:40 +0530

    update

--- ListExample.java
+++ ListExample.java
@@ -9,7 +9,6 @@ public class ListExample {
         numbers.add(2);
         numbers.add(3);
         numbers.add(4);
-        numbers.add(5);
 
         numbers.forEach(n -> System.out.println(n));
 
