a2d9881944f35d7f1f3a32da24b5bea4c9baf798
saiteja-vtl <saiteja@vebytechlabs.onmicrosoft.com>
2024-07-22 09:06:08 +0530

    update

---
+++ ListExample.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        numbers.forEach(n -> System.out.println(n));
+
+        numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+
+        numbers.stream().map(n -> n * n).forEach(System.out::println);
+    }
+}
+
